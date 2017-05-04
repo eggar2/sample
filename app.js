2b31a5556fe803efe1d56e483071e84d5fdeefff
Eggar Cortez <eggarcortez@Roxmons.local>
2017-05-04 17:17:51 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+
+// USE
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+	console.log(req.body)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
